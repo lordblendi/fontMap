d2ddefd13a118ec943352d77918a7aed247a373d
Nóra Szepes <nora.szepes@tenforce.com>
2018-03-05 13:20:03 +0100

    added theme class selection for selector.js

--- scripts/selector.js
+++ scripts/selector.js
@@ -9,7 +9,7 @@ $('.JS_selectorItem').on('click', function() {
   const selector = selectorValue.closest('.JS_selector');
 
   // only if this is not a multiselect popup toggle
-  if(!selector.hasClass('MULTISELECT__POPUP')) {
+  if (!selector.hasClass('MULTISELECT__POPUP')) {
     handleSelector(selector, selectorValue);
   }
 
@@ -29,26 +29,39 @@ function handleSelector(selector, selectorValue) {
     // if required, remove all the others from being selected
     selectedValues.removeClass('pmx-selector__item--selected');
     // if required, select the one you clicked on
-    if(required === true) {
+    if (required === true) {
       selectorValue.addClass('pmx-selector__item--selected');
     }
     // otherwise if it's not selected, toggle it;
     else {
-      if(!isSelected) {
+      if (!isSelected) {
         selectorValue.toggleClass('pmx-selector__item--selected');
       }
     }
-  }
-  else if (multiSelect === true) {
+
+    // TEMPORARY THEME SELECTOR
+    // get current css classes and find the one that's starts with theme_
+    var currentClasses = $('.fontMap_characters')[0].className.split(' ');
+    var currentTheme = "";
+    $.each(currentClasses, function(index, appliedClass) {
+      if (appliedClass.indexOf('theme_') >= 0) {
+        currentTheme = appliedClass;
+        return;
+      }
+    });
+    var newTheme = "theme_" + selectorValue.attr('data-theme');
+    // remove old theme, add new theme
+    $('.fontMap_characters').removeClass(currentTheme).addClass(newTheme);
+
+  } else if (multiSelect === true) {
     // if required
     // only allow toggle, if the this is not the last checked one
-    if(required === true) {
-      if(selectedValues.length === 1) {
+    if (required === true) {
+      if (selectedValues.length === 1) {
         if (isSelected === false) {
           selectorValue.toggleClass('pmx-selector__item--selected');
         }
-      }
-      else {
+      } else {
         selectorValue.toggleClass('pmx-selector__item--selected');
       }
     }
