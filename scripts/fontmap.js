91104cd366068dcd66b249fd0b155c54266d5210
Nóra Szepes <nora.szepes@tenforce.com>
2018-05-08 15:05:30 +0200

    close popup when click outside

--- scripts/fontmap.js
+++ scripts/fontmap.js
@@ -1,48 +1,58 @@
 ---
 ---
 
-$(document).ready(function() {
-  opentype.load('{{ site.baseurl }}/assets/fonts/tenforce.ttf', function(err, font) {
-    if (err) {
-      alert('Could not load font: ' + err);
-    } else {
-      const glyphs = font.glyphs.glyphs;
-      const fontMapCharacters = $('.fonticons')
+opentype.load('{{ site.baseurl }}/assets/fonts/tenforce.ttf', function(err, font) {
+  if (err) {
+    alert('Could not load font: ' + err);
+  } else {
+    const glyphs = font.glyphs.glyphs;
+    const fontMapCharacters = $('.fonticons')
 
-      $.each(glyphs, function(index, glyph){
-        if ( index >= 1435 && index <= 7000) {
-          let glyphData = getGlyphData(index, glyph);
-          let element = document.createElement('li');
-          $(element).html(glyphData).addClass('fonticons__item');
-          fontMapCharacters.append(element);
+    $.each(glyphs, function(index, glyph) {
+      if (index >= 1435 && index <= 7000) {
+        let glyphData = getGlyphData(index, glyph);
+        let element = document.createElement('li');
+        $(element).html(glyphData).addClass('fonticons__item');
+        fontMapCharacters.append(element);
+      }
+    });
+
+    $('.fonticons__item').click(function() {
+      var theme = $(this).parent()[0].className;
+      var icon = $(this).find('.fonticons__item--charcode').html();
+      var code = $(this).find('.fonticons__item--unicode').html();
+      $('.icon-pop').find('.icon__inner').html(icon);
+      $('.icon-pop').find('.icon__code').html(code);
+      $('.icon-pop').addClass('icon-pop--visible ' + theme);
+    })
+
+    $('.icon-pop').click(function(event) {
+      const pop = $(this);
+      if (pop.hasClass('icon-pop--visible')) {
+        const clickX = event.clientX;
+        const clickY = event.clientY;
+
+        const popup = $('.icon-pop__inner')[0];
+        const popupWidth = popup.offsetWidth;
+        const popupHeight = popup.offsetHeight;
+        const popupY = popup.offsetTop;
+        const popupX = popup.offsetLeft;
+        const popupBottom = popupY + popupHeight;
+        const popupRight = popupX + popupWidth
+
+        if (
+          clickX < popupX ||
+          clickX > popupRight ||
+          clickY < popupY ||
+          clickY > popupBottom
+        ) {
+          $(pop).removeClass('icon-pop--visible');
         }
-      });
+      }
+    });
 
-      $('.fonticons__item').click(function() {
-        var theme = $(this).parent()[0].className;
-        var icon = $(this).find('.fonticons__item--charcode').html();
-        var code = $(this).find('.fonticons__item--unicode').html();
-        $('.icon-pop').find('.icon__inner').html(icon);
-        $('.icon-pop').find('.icon__code').html(code);
-        $('.icon-pop').addClass('icon-pop--visible ' + theme);
-      })
-
-      $('.icon-pop--visible').click(function(event) {
-        console.log(event.target);
-    /*
-        if (!$(event.target).is('.icon-pop__inner')) {
-          $(this).removeClass('icon-pop--visible');
-        }
-    */
-      });
-
-    }
-  });
-
-
-
+  }
 });
-
 
 
 function getGlyphData(index, glyph) {
